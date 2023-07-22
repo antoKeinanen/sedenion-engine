2534514cebb24c995e0e2bd8788b6c38d6971131
antoKeinanen <anto.keinanen@gmail.com>
2023-07-22 18:30:30 +0300

    feat: add wasm binds

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+use wasm_bindgen::prelude::*;
+
 mod error;
 pub mod numeric_evaluator;
 mod math;
+
+#[wasm_bindgen]
+pub fn evaluate(expression: &str) -> f64 {
+    numeric_evaluator::evaluate(expression).unwrap()
+}
