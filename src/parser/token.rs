f54d0710b4c922610bbe5a82091128ad68e50537
antoKeinanen <anto.keinanen@gmail.com>
2023-08-04 12:12:45 +0300

    feat: add recursive single node optimizer

--- src/parser/token.rs
+++ src/parser/token.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum Expr {
     Number(f64),
     UnaryMinus(Box<Expr>),
@@ -13,7 +13,7 @@ pub enum Expr {
     },
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Op {
     Add,
     Subtract,
@@ -21,6 +21,184 @@ pub enum Op {
     Divide,
     Modulo,
     Power,
+}
+
+impl Expr {
+    fn optimize_node(&self) -> Expr {
+        match self {
+            Expr::UnaryMinus(inner) => {
+                let inner = *inner.to_owned();
+                // -(-a) => a
+                if let Expr::UnaryMinus(inner_inner) = inner {
+                    return *inner_inner;
+                }
+
+                // -0 = 0
+                if let Expr::Number(inner_n) = inner {
+                    if inner_n == 0.0 {
+                        return Expr::Number(0.0);
+                    }
+                }
+
+                Expr::UnaryMinus(Box::new(inner.optimize_node()))
+            }
+            Expr::BinOp { lhs, op, rhs } => {
+                let optimized_lhs = lhs.optimize_node();
+                let optimized_rhs = rhs.optimize_node();
+
+                // 0 + a = a
+                if let (Expr::Number(num), Op::Add) = (&optimized_lhs, &op) {
+                    if num == &0.0 {
+                        return optimized_rhs;
+                    }
+                }
+
+                // a + 0 = a
+                if let (Expr::Number(num), Op::Add) = (&optimized_rhs, &op) {
+                    if num == &0.0 {
+                        return optimized_lhs;
+                    }
+                }
+
+                // a - a = 0
+                if let Op::Subtract = op {
+                    if optimized_lhs == optimized_rhs {
+                        return Expr::Number(0.0);
+                    }
+                }
+
+                // 0 - a = a
+                if let (Expr::Number(num), Op::Subtract) = (&optimized_lhs, &op) {
+                    if num == &0.0 {
+                        return optimized_rhs;
+                    }
+                }
+
+                // a - 0 = a
+                if let (Expr::Number(num), Op::Subtract) = (&optimized_rhs, &op) {
+                    if num == &0.0 {
+                        return optimized_lhs;
+                    }
+                }
+
+                // 1 * a = a
+                if let (Expr::Number(num), Op::Multiply) = (&optimized_lhs, &op) {
+                    if num == &1.0 {
+                        return optimized_rhs;
+                    }
+                }
+
+                // a * 1 = a
+                if let (Expr::Number(num), Op::Multiply) = (&optimized_rhs, &op) {
+                    if num == &1.0 {
+                        return optimized_lhs;
+                    }
+                }
+
+                // 0 * a = 0
+                if let (Expr::Number(num), Op::Multiply) = (&optimized_lhs, &op) {
+                    if num == &0.0 {
+                        return Expr::Number(0.0);
+                    }
+                }
+
+                // a * 0 = 0
+                if let (Expr::Number(num), Op::Multiply) = (&optimized_rhs, &op) {
+                    if num == &0.0 {
+                        return Expr::Number(0.0);
+                    }
+                }
+
+                // a * a = a^2
+                if let Op::Multiply = op {
+                    if optimized_lhs == optimized_rhs {
+                        return Expr::BinOp {
+                            lhs: Box::new(optimized_lhs),
+                            op: Op::Power,
+                            rhs: Box::new(Expr::Number(2.0)),
+                        };
+                    }
+                }
+
+                // a^b * a^c = a^(b+c)
+                if let Op::Multiply = op {
+                    if let (
+                        Expr::BinOp {
+                            lhs: left_lhs,
+                            op: left_operator,
+                            rhs: left_rhs,
+                        },
+                        Expr::BinOp {
+                            lhs: right_lhs,
+                            op: right_operator,
+                            rhs: right_rhs,
+                        },
+                    ) = (&optimized_lhs, &optimized_rhs)
+                    {
+                        if let (Op::Power, Op::Power) = (left_operator, right_operator) {
+                            if left_lhs == right_lhs {
+                                return Expr::BinOp {
+                                    lhs: Box::new(*left_lhs.to_owned()),
+                                    op: Op::Power,
+                                    rhs: Box::new(Expr::BinOp {
+                                        lhs: left_rhs.to_owned(),
+                                        op: Op::Add,
+                                        rhs: right_rhs.to_owned(),
+                                    }),
+                                };
+                            }
+                        }
+                    }
+                }
+
+                // a^1 = a
+                if let (Expr::Number(n), Op::Power) = (&optimized_rhs, op) {
+                    if n == &1.0 {
+                        return optimized_lhs;
+                    }
+                }
+
+                // a^-n = 1/(a^n)
+                if let (Expr::UnaryMinus(inner), Op::Power) = (&optimized_rhs, op) {
+                    let inner = *inner.to_owned();
+
+                    if let Expr::Number(n) = inner {
+                        return Expr::BinOp {
+                            lhs: Box::new(Expr::Number(1.0)),
+                            op: Op::Divide,
+                            rhs: Box::new(Expr::BinOp {
+                                lhs: Box::new(optimized_lhs),
+                                op: Op::Power,
+                                rhs: Box::new(Expr::Number(n)),
+                            }),
+                        };
+                    }
+                }
+
+                // a / 1 = a
+                if let (Expr::Number(num), Op::Divide) = (&optimized_rhs, &op) {
+                    if num == &1.0 {
+                        return optimized_lhs;
+                    }
+                }
+
+                // a / a = 1
+                if let Op::Divide = op {
+                    if optimized_lhs == optimized_rhs {
+                        return Expr::Number(1.0);
+                    }
+                }
+
+                Expr::BinOp {
+                    lhs: Box::new(optimized_lhs),
+                    op: *op,
+                    rhs: Box::new(optimized_rhs),
+                }
+            }
+            Expr::Number(n) => Expr::Number(*n),
+            token => todo!("Optimizing for '{token:?}' not implemented yet!"),
+        }
+    }
 }
 
 impl ToString for Expr {
